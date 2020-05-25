41c9e9aaef2f9f5d9c22352cdcc6650cf9933eeb
OrlandoEduardo101 <orlando.eduardo101@gmail.com>
2020-05-25 12:27:09 -0300

    atualizar dados perfil

--- lib/configuracoes.dart
+++ lib/configuracoes.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsflutter/res.dart';
 import 'package:image_picker/image_picker.dart';
@@ -12,6 +15,16 @@ class _ConfigsState extends State<Configs> {
 
   TextEditingController _controllerNome = TextEditingController();
   File _img;
+  String _idLog;
+  bool _subindo = false;
+  String _urlRec;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _recuperarDados();
+  }
 
   Future _recuperarIMG(String origem)async{
       File imagemSelecionada;
@@ -26,8 +39,82 @@ class _ConfigsState extends State<Configs> {
       }
       setState(() {
         _img = imagemSelecionada;
+        if (_img != null) {
+          _subindo = true;
+          _uploadIMG();
+        }
       });
   }
+
+  Future _uploadIMG()async{
+    FirebaseStorage storage = FirebaseStorage.instance;
+    StorageReference raiz = storage.ref();
+    StorageReference arq = raiz.child("perfil").child(_idLog+".jpg");
+    //upload img
+    StorageUploadTask task = arq.putFile(_img);
+    //controle progresso
+    task.events.listen((StorageTaskEvent event) { 
+      if (event.type == StorageTaskEventType.progress) {
+        setState(() {
+          _subindo = true;
+        });
+      }  else if(event.type == StorageTaskEventType.success){
+        setState(() {
+          _subindo = false;
+        });
+      }
+    });
+    //recuperar url da img
+    task.onComplete.then((StorageTaskSnapshot value){
+      _recuperarURL(value);
+    });
+  }
+
+  Future _recuperarURL(StorageTaskSnapshot snapshot) async{
+    String url = await snapshot.ref.getDownloadURL();
+    //print(url);
+    _atualizarURL(url);
+    setState(() {
+      _urlRec = url;
+    });
+  }
+
+  _atualizarURL(String url){
+    Firestore db = Firestore.instance;
+    Map<String, dynamic> dadosAtt = {
+      "urlIMG" : url
+    };
+    db.collection("users").document(_idLog).updateData(dadosAtt);
+  }
+
+  _atualizarNome(){
+    String nome = _controllerNome.text;
+    Firestore db = Firestore.instance;
+    Map<String, dynamic> dadosAtt = {
+      "nome" : nome
+    };
+    db.collection("users").document(_idLog).updateData(dadosAtt);
+  }
+
+  _recuperarDados()async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirebaseUser userLog = await auth.currentUser();
+    _idLog = userLog.uid;
+    Firestore db = Firestore.instance;
+    DocumentSnapshot snapshot = await db.collection("users").document(_idLog).get();
+
+    Map<String, dynamic> dados = snapshot.data;
+    _controllerNome.text = dados["nome"];
+    if (dados["urlIMG"] != null) {
+      //_urlRec = dados["urlIMG"];
+      setState(() {
+        _urlRec = dados["urlIMG"];
+      });
+    }  
+    
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -41,10 +128,13 @@ class _ConfigsState extends State<Configs> {
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                //Carregando
+                Container(
+                  padding: EdgeInsets.all(16),
+                  child: _subindo ? CircularProgressIndicator() : Container(),
+                ),
                 CircleAvatar(
                   radius: 100,
-                  backgroundImage: NetworkImage(Res.jesusURL),
+                  backgroundImage: _urlRec != null ? NetworkImage(_urlRec) : null,
                   backgroundColor: Colors.grey,
                 ),
                 Row(
@@ -89,7 +179,7 @@ class _ConfigsState extends State<Configs> {
                           borderRadius: BorderRadius.circular(32)
                       ),
                       onPressed: () {
-
+                        _atualizarNome();
                       }),
                 ),
               ],
