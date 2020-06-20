fea115eaf2d268db0bb30a78bd1da5348235ab94
OrlandoEduardo101 <orlando.eduardo101@gmail.com>
2020-06-20 01:20:47 -0300

    adicionando o observer á barra de digitar mensagem na tela de mensagens

--- lib/app/modules/mensagens/mensagens_page.dart
+++ lib/app/modules/mensagens/mensagens_page.dart
@@ -36,101 +36,103 @@ class _MensagensPageState
 
   @override
   Widget build(BuildContext context) {
-    var caixaMsg = Container(
-      padding: EdgeInsets.all(8),
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(right: 8),
-              child: TextField(
-                autofocus: true,
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
-                    hintText: "Digite uma mensagem...",
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32),
-                    ),
-                    prefixIcon: controller.subindo ? CircularProgressIndicator() : IconButton(
-                        icon: Icon(Icons.camera_alt), onPressed: controller.enviarFoto)),
-                controller: controller.controllerMsg,
+    var caixaMsg = Observer(builder: (_){
+      return Container(
+        padding: EdgeInsets.all(8),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(right: 8),
+                child: TextField(
+                  autofocus: true,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                      hintText: "Digite uma mensagem...",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                      prefixIcon: controller.subindo ? CircularProgressIndicator() : IconButton(
+                          icon: Icon(Icons.camera_alt), onPressed: controller.enviarFoto)),
+                  controller: controller.controllerMsg,
+                ),
               ),
             ),
-          ),
-          Platform.isIOS ? CupertinoButton(
-              child: Text("enviar"),
-              onPressed: controller.enviarMsg) :
-          FloatingActionButton(
-              backgroundColor: Color(0xff075E54),
-              child: Icon(
-                Icons.send,
-                color: Colors.white,
-              ),
-              mini: true,
-              onPressed: controller.enviarMsg),
-        ],
-      ),
-    );
+            Platform.isIOS ? CupertinoButton(
+                child: Text("enviar"),
+                onPressed: controller.enviarMsg) :
+            FloatingActionButton(
+                backgroundColor: Color(0xff075E54),
+                child: Icon(
+                  Icons.send,
+                  color: Colors.white,
+                ),
+                mini: true,
+                onPressed: controller.enviarMsg),
+          ],
+        ),
+      );
+    });
 
     var stream = Observer(
         builder: (_){
-            if(controller.mensagens==null || controller.mensagens.data==null || controller.idLog == null) { print('conversas');
-              return Expanded(
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Text("Carregando conversas",
-                          style: TextStyle(color: Colors.white),),
-                        CircularProgressIndicator(),
-                      ],
-                    ),
-                  )
-              );
-            }
-            if(controller.mensagens.hasError){print('erro');
-                  return Expanded(
-                    child: Text("erro"),
-                  );
-                }
-              else {
-                  print('AAAAAAAAAAAAAA');
-                  List<MensagemModel> list = controller.mensagens.data;
-                  print("queryyy: "+list.toString());
-                  return Expanded(
-                    child: ListView.builder(
-                        controller: controller.scrollController,
-                        itemCount: list.length,
-                        itemBuilder: (context, index) {
-                          MensagemModel msgs = MensagemModel();
-                          msgs = list[index];
-                          double largura = MediaQuery.of(context).size.width * 0.8;
-                          Alignment alinhamento = Alignment.centerRight;
-                          Color cor = Color(0xffd2ffa5);
-                          if (controller.idLog != msgs.idUser) {
-                            cor = Colors.white;
-                            alinhamento = Alignment.bottomLeft;
-                          }
-                          return Align(
-                            alignment: alinhamento,
-                            child: Padding(
-                              padding: EdgeInsets.all(6),
-                              child: Container(
-                                width: largura,
-                                padding: EdgeInsets.all(16),
-                                decoration: BoxDecoration(
-                                    color: cor,
-                                    borderRadius:
-                                    BorderRadius.all(Radius.circular(8))),
-                                child: msgs.tipo == "text" ? Text(msgs.msg, style: TextStyle(fontSize: 18)) : Image.network(msgs.urlIMG),
-                              ),
+          if(controller.mensagens==null || controller.mensagens.data==null || controller.idLog == null) { print('conversas');
+            return Expanded(
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text("Carregando conversas",
+                        style: TextStyle(color: Colors.white),),
+                      CircularProgressIndicator(),
+                    ],
+                  ),
+                )
+            );
+          }
+          if(controller.mensagens.hasError){print('erro');
+                return Expanded(
+                  child: Text("erro"),
+                );
+              }
+            else {
+                print('AAAAAAAAAAAAAA');
+                List<MensagemModel> list = controller.mensagens.data;
+                print("queryyy: "+list.toString());
+                return Expanded(
+                  child: ListView.builder(
+                      controller: controller.scrollController,
+                      itemCount: list.length,
+                      itemBuilder: (context, index) {
+                        MensagemModel msgs = MensagemModel();
+                        msgs = list[index];
+                        double largura = MediaQuery.of(context).size.width * 0.8;
+                        Alignment alinhamento = Alignment.centerRight;
+                        Color cor = Color(0xffd2ffa5);
+                        if (controller.idLog != msgs.idUser) {
+                          cor = Colors.white;
+                          alinhamento = Alignment.bottomLeft;
+                        }
+                        return Align(
+                          alignment: alinhamento,
+                          child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: Container(
+                              width: largura,
+                              padding: EdgeInsets.all(16),
+                              decoration: BoxDecoration(
+                                  color: cor,
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(8))),
+                              child: msgs.tipo == "text" ? Text(msgs.msg, style: TextStyle(fontSize: 18)) : Image.network(msgs.urlIMG),
                             ),
-                          );
-                        }),
-                  );
+                          ),
+                        );
+                      }),
+                );
           }
         });
 
